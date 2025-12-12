ce5a3d677b005a4117a14f4a91c96c6988fdda61
aleksanderpalamar <apalamardev@gmail.com>
2025-12-12 10:54:50 -0300

    Refactor Search.sh to improve error handling and configuration retrieval

--- scripts/Search.sh
+++ scripts/Search.sh
@@ -3,17 +3,19 @@
 config_file=$HOME/.config/hypr/UserConfigs/UserDefaults.conf
 
 if [[ ! -f "$config_file" ]]; then
-  echo "Error: Configuration file not found!"
-  exit 1
+    echo "Error: Configuration file not found!"
+    exit 1
 fi
 
-config_content=$(sed 's/\$//g' "$config_file" | sed 's/ = /=/')
+get_config() {
+    local var_name="$1"
+    grep "^\s*\$$var_name\s*=" "$config_file" | sed 's/^.*=//;s/#.*//' | xargs | sed 's/^"//;s/"$//'
+}
 
-eval "$config_content"
+Search_Engine=$(get_config "Search_Engine")
 
 if [[ -z "$Search_Engine" ]]; then
-    echo "Error: \$Search_Engine is not set in the configuration file!"
-    exit 1
+    Search_Engine="https://duckduckgo.com/?q={}"
 fi
 
 rofi_theme="$HOME/.config/rofi/config-search.rasi"
@@ -26,7 +28,6 @@ fi
 
 query=$(echo "" | rofi -dmenu -config "$rofi_theme" -mesg "$msg")
 
-# Força refresh do Hyprland para limpar qualquer overlay residual
 hyprctl dispatch focuscurrentorlast
 
 if [[ -n "$query" ]]; then
